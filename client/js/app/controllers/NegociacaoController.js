5f25ec892e6cbbf74dfe37b8bbfcd93a061884f1
TCE\felipe.augusto <felipe.augusto@tce.ce.gov.br>
2017-07-27 12:08:26 -0300

    Estado do projeto ao final do Curso 2

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,27 +6,60 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
-       
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
+      
+        this._listaNegociacoes = new Bind(
+            new ListaNegociacoes(),
+            new NegociacoesView($('#negociacoesView')),
+            'adiciona', 
+            'esvazia'
+        );
 
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
+        this._mensagem = new Bind(
+            new Mensagem(),
+            new MensagemView($('#mensagemView')),
+            'texto'            
+        );
 
     }
 
     adiciona (event) {
         
         event.preventDefault();
-
-        this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
-        this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._mensagemView.update(this._mensagem);
+        this._listaNegociacoes.adiciona(this._criaNegociacao());        
+        this._mensagem.texto = 'Negociação adicionada com sucesso';        
         this._limpaFormulario();
+
+    }
+
+
+    importaNegociacoes() {
+
+        let service = new NegociacaoService();
+
+        Promise.all([
+            
+            service.obterNegociacoesDaSemana(),
+            service.obterNegociacoesDaSemanaAnterior(),
+            service.obterNegociacoesDaSemanaRetrasada()
         
+        ]).then ( negociacoes => {
+
+            negociacoes
+                .reduce( (arrayAchatado, array) => arrayAchatado.concat(array), [] )
+                .forEach( negociacao => this._listaNegociacoes.adiciona(negociacao) );
+            this._mensagem.texto = 'Negociações importadas com sucesso';
+
+        })
+        .catch( erro => this._mensagem.texto = erro );        
+
+    }
+
+
+    apaga() {
+        
+        this._listaNegociacoes.esvazia();
+        this._mensagem.texto = 'Negociações apagadas com sucesso';
+
     }
 
     _criaNegociacao() {
---
+++ client/js/app/services/ProxyFactory.js
@@ -0,0 +1,45 @@
+class ProxyFactory {
+
+    static create(objeto, props, acao) {
+
+        return new Proxy( objeto, {
+
+            // executado sempre quando qualquer propriedade for lida
+            get (target, prop, receiver) {
+
+                if( props.includes(prop) && ProxyFactory._ehFuncao(target[prop])) {
+
+                    return function() {
+
+                        console.log(`interceptando ${prop}`);
+                        Reflect.apply(target[prop], target, arguments);
+                        return acao(target);
+                    }
+                
+                }
+
+                return Reflect.get(target, prop, receiver);
+
+            },
+
+            set (target, prop, value, receiver) {
+
+                if(props.includes(prop)) {
+                    target[prop] = value;
+                    acao(target);
+                }    
+                return Reflect.set(target, prop, value, receiver);
+
+            }
+
+        });
+
+    }
+
+    static _ehFuncao(func) {
+
+        return typeof(func) == typeof(Function); 
+
+    }
+
+}
